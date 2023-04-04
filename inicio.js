57ef0a665521433e59b80f5a26716e6ff60d03a3
GabrielSilveira07~ <gabrielluisgsilveira07@gmail.com>
2023-04-03 21:56:38 -0300

    calculadora.js

---
+++ inicio.js
@@ -0,0 +1,31 @@
+var calculadora = require("./calculadora");
+
+
+
+console.log(calculadora.mult(15, 5));
+console.log(calculadora.soma(5, 5));
+
+console.log("teste");
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var mostrarSite = true;
+const site = "guia Node.js"
+
+console.log("hello word");
+console.log("meu nome é gabriel")
+console.log("estou aprendendo Node.js")
+
+if(mostrarSite){  // se mostrar site
+    console.log(site) //Printe site
+}*/
